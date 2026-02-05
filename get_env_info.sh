88ca6cd3d366ac7e986a8950f6eb2561352e0c84
Sky_High <1901532022@qq.com>
2026-02-05 15:12:49 +0800

    cleanup_structure

--- get_env_info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-KEY_PATH="/mnt/c/Users/Administrator/.ssh/ksy.id"
-cp "$KEY_PATH" /tmp/ksy.id && chmod 600 /tmp/ksy.id
-ssh -p 2222 -i /tmp/ksy.id -o StrictHostKeyChecking=no qtc_yu@8.145.51.96 "nvidia-smi && /usr/local/cuda/bin/nvcc --version"
-rm /tmp/ksy.id
